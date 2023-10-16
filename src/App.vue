317e31c1e0521147eba23091614ffae06a04c0d2
Lativ24 <128636479+Lativ24@users.noreply.github.com>
2023-10-16 16:22:53 -0600

    Nuevos cambios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
 <main>
-  <div class="search-box">
-    <input 
-    type="text" 
-    class="search-bar"
-    placeholder="Search..."
-    v-model="query"
-    @keypress="fetchWeather"
-    />
-  </div>
+  <search-box @search="fetchWeather"></search-box>
   <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
     <div class="location-box">
       <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
@@ -25,9 +17,12 @@
 </template>
 
 <script>
-
+import SearchBox from './components/SearchBox.vue'; // Asegúrate de importar el componente
 export default {
   name: 'app',
+  components: {
+    'search-box': SearchBox, 
+  },
   data(){
     return{
       api_key:'51549e0c5273fe51c515c1a71caa5e95',
@@ -37,13 +32,12 @@ export default {
     }
   },
   methods:{
-    fetchWeather(e){
-      if(e.key=="Enter"){
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&&APPID=${this.api_key}`)
-        .then(res =>  {
-          return res.json();
-        }).then(this.setResults)
-      }
+    fetchWeather(location){
+      
+      fetch(`${this.url_base}weather?q=${location}&units=metric&&APPID=${this.api_key}`)
+        .then((res) => res.json())
+        .then(this.setResults)
+      
     },
     setResults(results){
       this.weather=results;
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search-box">
+      <input
+        type="text"
+        class="search-bar"
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        query: '', 
+      };
+    },
+    methods: {
+      fetchWeather(e) {
+        if (e.key === 'Enter') {
+          this.$emit('search', this.query); 
+        }
+      },
+    },
+  };
+  </script>
+  
